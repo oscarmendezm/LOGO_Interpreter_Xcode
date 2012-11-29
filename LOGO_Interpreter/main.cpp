da0dc7e87b7c9ab8aaf56c237106d8f5cd38c91b
Oscar Mendez <o.mendez.m@gmail.com>
2012-11-29 19:30:49 +0000

    Initial Commit

---
+++ LOGO_Interpreter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LOGO_Interpreter
+//
+//  Created by Oscar Alejandro Mendez Maldonado on 29/11/2012.
+//  Copyright (c) 2012 Oscar Alejandro Mendez Maldonado. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
